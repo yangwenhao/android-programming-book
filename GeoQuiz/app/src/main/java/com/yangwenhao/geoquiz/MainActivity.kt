4b56dd75aafc159bf4c199c42198acdeb6422b79
Wenhao Yang <yangwenhaogdut@163.com>
2022-03-31 22:48:23 +0800

    update

--- GeoQuiz/app/src/main/java/com/yangwenhao/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/yangwenhao/geoquiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.yangwenhao.geoquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -18,13 +19,14 @@ private const val KEY_INDEX = "index"
 private const val KEY_VIS_STATUS = "visStatus"
 private const val KEY_SCORE = "score"
 private const val KEY_ANSWERED = "answered"
-
+private const val REQUEST_CODE_CHEAT = 0
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: ImageButton
     private lateinit var prevButton: ImageButton
+    private lateinit var cheatButton: Button
     private lateinit var questionTextView: TextView
 
     private val quizViewModel : QuizViewModel by lazy {
@@ -52,7 +54,8 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
         prevButton = findViewById(R.id.prev_button)
-        // questionTextView = findViewById(R.id.question_text_view)
+        cheatButton = findViewById(R.id.cheat_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
         setTrueFalseButtonVis()
 
@@ -71,7 +74,11 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
             setTrueFalseButtonVis()
         }
-
+        cheatButton.setOnClickListener {
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            startActivityForResult(intent, REQUEST_CODE_CHEAT)
+        }
         prevButton.setOnClickListener {
             quizViewModel.moveToPrev()
             updateQuestion()
@@ -138,8 +145,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setTrueFalseButtonVis() {
-        trueButton.setVisibility(quizViewModel.getButVis());
-        falseButton.setVisibility(quizViewModel.getButVis());
+        trueButton.visibility = quizViewModel.getButVis()
+        falseButton.visibility = quizViewModel.getButVis()
     }
 
     private fun showScore() {
