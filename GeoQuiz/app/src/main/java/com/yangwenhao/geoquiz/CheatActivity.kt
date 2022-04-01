9aa22713812e303ede9b2d263a1cf64c4883843a
Wenhao Yang <yangwenhaogdut@163.com>
2022-04-01 23:13:15 +0800

    update

--- GeoQuiz/app/src/main/java/com/yangwenhao/geoquiz/CheatActivity.kt
+++ GeoQuiz/app/src/main/java/com/yangwenhao/geoquiz/CheatActivity.kt
@@ -5,25 +5,36 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 const val EXTRA_ANSWER_SHOWN = "com.yangwenhao.geoquiz.answer"
 private const val EXTRA_ANSWER_IS_TRUE = "com.yangwenhao.geoquiz.answer_is_true"
+private const val KEY_IS_CHEAT = "isCheat"
+private const val KEY_ANSWER_TEXT = "answerText"
 
 class CheatActivity : AppCompatActivity() {
 
     private lateinit var answerTextView: TextView
     private lateinit var showAnswerButton: Button
-    private var answerIsTrue = false
+    private var isCheat = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheat)
 
-        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+        isCheat = savedInstanceState?.getBoolean(KEY_IS_CHEAT, false) ?: false
+        if (isCheat) {
+            setAnswerShownResult(true)
+        }
+        val answerText = savedInstanceState?.getCharSequence(KEY_ANSWER_TEXT, "") ?: ""
+        var answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
         answerTextView = findViewById(R.id.answer_text_view)
         showAnswerButton = findViewById(R.id.show_answer_button)
+
+        answerTextView.setText(answerText)
 
         showAnswerButton.setOnClickListener {
             val answerText = when {
@@ -31,12 +42,19 @@ class CheatActivity : AppCompatActivity() {
                 else -> R.string.false_button
             }
             answerTextView.setText(answerText)
+            isCheat = true
             setAnswerShownResult(true)
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(KEY_IS_CHEAT, isCheat)
+        outState.putCharSequence(KEY_ANSWER_TEXT, answerTextView.text)
+    }
+
     private fun setAnswerShownResult(isAnswerShown: Boolean) {
-        val data = Intent().apply {
+        var data = Intent().apply {
             putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
         }
         setResult(Activity.RESULT_OK, data)
