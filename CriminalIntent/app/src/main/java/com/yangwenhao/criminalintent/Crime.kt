f208e18304e5227ad8beb357e8ff9fae83878c7d
Wenhao Yang <yangwenhaogdut@163.com>
2022-04-02 22:37:18 +0800

    新建CriminalIntent项目

---
+++ CriminalIntent/app/src/main/java/com/yangwenhao/criminalintent/Crime.kt
@@ -0,0 +1,10 @@
+package com.yangwenhao.criminalintent
+
+import java.util.*
+
+data class Crime(
+    val id: UUID = UUID.randomUUID(),
+    var title: String = "",
+    var data: Date = Date(),
+    var isSolved: Boolean = false
+)
