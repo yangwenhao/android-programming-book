9af5888224a1df1e5b47b34efc40c965aac37287
Wenhao Yang <yangwenhaogdut@163.com>
2022-05-04 22:32:52 +0800

    update

--- beatbox/app/src/main/java/com/yangwenhao/android/beatbox/MainActivity.kt
+++ beatbox/app/src/main/java/com/yangwenhao/android/beatbox/MainActivity.kt
@@ -10,20 +10,41 @@ import com.yangwenhao.android.beatbox.databinding.ActivityMainBinding
 import com.yangwenhao.android.beatbox.databinding.ListItemSoundBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var beatBox: BeatBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        beatBox = BeatBox(assets)
+
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.recyclerView.apply {
             layoutManager = GridLayoutManager(context, 3)
-            adapter = SoundAdapter()
+            adapter = SoundAdapter(beatBox.sounds)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        beatBox.release()
     }
 
     private inner class SoundHolder(private val binding: ListItemSoundBinding): RecyclerView.ViewHolder(binding.root) {
 
+        init {
+            binding.viewModel = SoundViewModel(beatBox)
+        }
+
+        fun bind(sound: Sound) {
+            binding.apply {
+                viewModel?.sound = sound
+                executePendingBindings()
+            }
+        }
     }
 
-    private inner class SoundAdapter() : RecyclerView.Adapter<SoundHolder>() {
+    private inner class SoundAdapter(private val sounds: List<Sound>) : RecyclerView.Adapter<SoundHolder>() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SoundHolder {
             val binding = DataBindingUtil.inflate<ListItemSoundBinding>(layoutInflater, R.layout.list_item_sound, parent,false)
@@ -31,9 +52,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onBindViewHolder(holder: SoundHolder, position: Int) {
-            TODO("Not yet implemented")
+            val sound = sounds[position]
+            holder.bind(sound)
         }
 
-        override fun getItemCount() = 0
+        override fun getItemCount() = sounds.size
     }
 }
---
+++ beatbox/app/src/test/java/com/yangwenhao/android/beatbox/SoundViewModelTest.kt
@@ -0,0 +1,34 @@
+package com.yangwenhao.android.beatbox
+
+import org.hamcrest.CoreMatchers.`is`
+
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+class SoundViewModelTest {
+
+    private lateinit var beatBox: BeatBox
+    private lateinit var sound: Sound
+    private lateinit var subject: SoundViewModel
+
+    @Before
+    fun setUp() {
+        beatBox = mock(BeatBox::class.java)
+        sound = Sound("assetPath")
+        subject = SoundViewModel(beatBox)
+        subject.sound = sound
+    }
+
+    @Test
+    fun exposesSoundNameAsTitle() {
+        org.hamcrest.MatcherAssert.assertThat(subject.title, `is`(sound.name))
+    }
+
+    @Test
+    fun callsBeatBoxPlayOnButtonClicked() {
+        subject.onButtonClicked()
+        verify(beatBox).play(sound)
+    }
+}
