48b966a38e226f8d6ecaf1502ac6db60d6856bc6
Wenhao Yang <yangwenhaogdut@163.com>
2022-05-11 22:24:21 +0800

    update

--- NerdLauncher/app/src/main/java/com/yangwenhao/android/nerdlauncher/NerdLauncherActivity.kt
+++ NerdLauncher/app/src/main/java/com/yangwenhao/android/nerdlauncher/NerdLauncherActivity.kt
@@ -1,14 +1,80 @@
 package com.yangwenhao.android.nerdlauncher
 
+import android.content.Intent
+import android.content.pm.ResolveInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.yangwenhao.android.nerdlauncher.databinding.ActivityNerdLauncherBinding
+
+private const val TAG = "NerdLauncherActivity"
 
 class NerdLauncherActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityNerdLauncherBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityNerdLauncherBinding.inflate(layoutInflater);
+        binding = ActivityNerdLauncherBinding.inflate(layoutInflater);
         setContentView(binding.root)
+        binding.appRecyclerView.layoutManager = LinearLayoutManager(this)
+        setupAdapter()
     }
+
+    private fun setupAdapter() {
+        val startupIntent = Intent(Intent.ACTION_MAIN).apply {
+            addCategory(Intent.CATEGORY_LAUNCHER)
+        }
+
+        val activities = packageManager.queryIntentActivities(startupIntent, 0)
+        activities.sortWith { a, b ->
+            String.CASE_INSENSITIVE_ORDER.compare(
+                a.loadLabel(packageManager).toString(),
+                b.loadLabel(packageManager).toString()
+            )
+        }
+        Log.i(TAG, "Found ${activities.size} activities");
+        binding.appRecyclerView.adapter = ActivityAdapter(activities)
+    }
+
+    private class ActivityHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val nameTextView = itemView as TextView
+        private lateinit var resolveInfo: ResolveInfo
+
+        fun bindActivity(resolveInfo: ResolveInfo) {
+            this.resolveInfo = resolveInfo
+            val packageManager = itemView.context.packageManager
+            val appName = resolveInfo.loadLabel(packageManager).toString()
+            nameTextView.text = appName
+        }
+    }
+
+    private class ActivityAdapter(val activities: List<ResolveInfo>) : RecyclerView.Adapter<ActivityHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActivityHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+            return ActivityHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ActivityHolder, position: Int) {
+            val resolveInfo = activities[position]
+            holder.bindActivity(resolveInfo)
+        }
+
+        override fun getItemCount(): Int {
+            return activities.size
+        }
+    }
+
+
+
+
 }
