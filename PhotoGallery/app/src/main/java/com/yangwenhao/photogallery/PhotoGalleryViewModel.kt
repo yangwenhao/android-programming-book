6e02263cacfa6bac4aec5b280a69d120f6757cc3
Wenhao Yang <yangwenhaogdut@163.com>
2022-05-23 22:44:55 +0800

    update

--- PhotoGallery/app/src/main/java/com/yangwenhao/photogallery/PhotoGalleryViewModel.kt
+++ PhotoGallery/app/src/main/java/com/yangwenhao/photogallery/PhotoGalleryViewModel.kt
@@ -1,24 +1,27 @@
 package com.yangwenhao.photogallery
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
 
-class PhotoGalleryViewModel : ViewModel() {
+class PhotoGalleryViewModel(private val app: Application) : AndroidViewModel(app) {
 
     private val flickrFetchr: FlickrFetchr = FlickrFetchr()
     val galleryItemLiveData: LiveData<List<GalleryItem>>
     private val mutableSearchTerm = MutableLiveData<String>()
 
     init {
-        mutableSearchTerm.value = "planets"
+        mutableSearchTerm.value = QueryPreferences.getStoredQuery(app)
         galleryItemLiveData = Transformations.switchMap(mutableSearchTerm) { searchTerm ->
-            flickrFetchr.searchPhotos(searchTerm)
+            if (searchTerm.isBlank()) {
+                flickrFetchr.fetchPhotos()
+            } else {
+                flickrFetchr.searchPhotos(searchTerm)
+            }
         }
     }
 
     fun fetchPhotos(query: String="") {
+        QueryPreferences.setStoredQuery(app, query)
         mutableSearchTerm.value = query
     }
 
